b9b34e4b4049c63cd80dd26146ec07f7b6e69c8c
HyunSeungBeom <ghfk100@naver.com>
2025-02-11 18:48:40 +0900

    chore: 배포

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,6 +14,8 @@ export default function LoginPage() {
   const [error, setError] = useState("");
   const router = useRouter();
 
+  //배포테스트
+
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
