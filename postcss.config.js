cc858204cff01a9af4bd03d5fa71fdf9ea2d5e75
HyunSeungBeom <ghfk100@naver.com>
2025-02-11 18:27:55 +0900

    chore: build

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,3 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+module.exports = {
+  plugins: {},
 };
-
-module.exports = config;
