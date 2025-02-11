c42203d4108b4cda6a2c8a551acef56272277570
HyunSeungBeom <ghfk100@naver.com>
2025-02-11 10:28:09 +0900

    chore

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // const accessToken = request.cookies.get("accessToken")?.value;
-  // const refreshToken = request.cookies.get("refreshToken")?.value;
-  // if (!accessToken && !refreshToken) {
-  //   return NextResponse.redirect(new URL("/login", request.url));
-  // }
-  // if (accessToken && refreshToken) {
-  //   return NextResponse.redirect(new URL("/rooms", request.url));
-  // }
-  // return NextResponse.next();
+  const accessToken = request.cookies.get("accessToken")?.value;
+  const refreshToken = request.cookies.get("refreshToken")?.value;
+
+  return NextResponse.next();
 }
 
-// export const config = {
-//   matcher: "/rooms",
-// };
+export const config = {
+  matcher: "/rooms",
+};
