68d60bc5b9cce7b84d039e274bfbf6dc7fd4d120
현승범 <62886997+HyunSeungBeom@users.noreply.github.com>
2025-03-06 16:30:19 +0900

    .

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -15,7 +15,7 @@ export default function QueryProvider({
         defaultOptions: {
           queries: {
             staleTime: 60 * 1000, // 1분
-            refetchOnWindowFocus: false, // 윈도우 포커스 시 자동 리패치 비활성화
+            refetchOnWindowFocus: false, // 윈도우 포커스 시 자동 리패치 비활성화 
           },
         },
       })
